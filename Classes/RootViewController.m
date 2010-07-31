33f09add5293b690bb6f0d26ef9200c16eef83de
Matthaus Litteken <mlitteken@Matthaus-Littekens-MacBook-Pro.local>
2010-07-31 11:57:25 -0700

    Stub methods added for when the other view is there.

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -21,6 +21,9 @@
 //  THE SOFTWARE.
 
 #import "RootViewController.h"
+#import "PlaceMarkAppDelegate.h"
+#import "PlaceMarkCell.h"
+#import "PlaceMark.h"
 
 
 @implementation RootViewController
@@ -79,23 +82,28 @@
 
 // Customize the number of rows in the table view.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+	PlaceMarkAppDelegate *placeMarkDelegate = (PlaceMarkAppDelegate *)[[UIApplication sharedApplication] delegate];
+    return placeMarkDelegate.placemarks.count;
 }
 
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString *CellIdentifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-    }
-    
-	// Configure the cell.
-
-    return cell;
+	
+	static NSString *MyIdentifier = @"MyIdentifier";
+	
+	PlaceMarkCell *cell = (PlaceMarkCell *)[tableView dequeueReusableCellWithIdentifier:MyIdentifier];
+	if (cell == nil) {
+		cell = [[[PlaceMarkCell alloc] initWithFrame:CGRectZero reuseIdentifier:MyIdentifier] autorelease];
+	}
+	
+	PlaceMarkAppDelegate *placeMarkAppDelegate = (PlaceMarkAppDelegate *)[[UIApplication sharedApplication] delegate];
+	PlaceMark *pm = [placeMarkAppDelegate.placemarks objectAtIndex:indexPath.row];
+	
+	[cell setPlaceMark:pm];
+	
+	// Set up the cell
+	return cell;
 }
 
 
@@ -177,3 +185,155 @@
 
 @end
 
+/*
+ 
+@implementation RootViewController
+@synthesize todoView;
+
+- (void) addTodo:(id)sender {
+	todoAppDelegate *appDelegate = (todoAppDelegate *)[[UIApplication sharedApplication] delegate];
+	
+	if (self.todoView == nil) {
+		TodoViewController *viewController = [[TodoViewController alloc]
+											  initWithNibName:@"TodoViewController" bundle:[NSBundle mainBundle]];
+		self.todoView = viewController;
+		[viewController release];
+	}
+	
+	Todo *todo = [appDelegate addTodo];
+	[self.navigationController pushViewController:self.todoView animated:YES];
+	self.todoView.todo = todo;
+	self.todoView.title = todo.text;
+	[self.todoView.todoText setText:todo.text];
+}
+
+- (void)viewDidLoad {
+	// Add the following line if you want the list to be editable
+	// self.navigationItem.leftBarButtonItem = self.editButtonItem;
+	self.title = @"Todo Items";
+	self.navigationItem.leftBarButtonItem = self.editButtonItem;
+	
+	UIBarButtonItem *btn = [[UIBarButtonItem alloc] initWithTitle:@"Add" 
+															style:UIBarButtonItemStyleBordered target:self action:@selector(addTodo:)];
+	self.navigationItem.rightBarButtonItem = btn;
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	todoAppDelegate *appDelegate = (todoAppDelegate *)[[UIApplication sharedApplication] delegate];
+    return appDelegate.todos.count;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	todoAppDelegate *appDelegate = (todoAppDelegate *)[[UIApplication sharedApplication] delegate];
+	Todo *todo = (Todo *)[appDelegate.todos objectAtIndex:indexPath.row];
+	
+	if(self.todoView == nil) {
+		TodoViewController *viewController = [[TodoViewController alloc] 
+											  initWithNibName:@"TodoViewController" bundle:[NSBundle mainBundle]];
+		self.todoView = viewController;
+		[viewController release];
+	}
+	
+	[self.navigationController pushViewController:self.todoView animated:YES];
+	self.todoView.todo = todo;
+	self.todoView.title = todo.text;
+	[self.todoView.todoText setText:todo.text];
+	
+	NSInteger priority = todo.priority - 1;
+	if(priority > 2 || priority < 0) {
+		priority = 1;
+	}
+	priority = 2 - priority;
+	
+	[self.todoView.todoPriority setSelectedSegmentIndex:priority];
+	
+	if(todo.status == 1) {
+		[self.todoView.todoButton setTitle:@"Mark As In Progress" forState:UIControlStateNormal];
+		[self.todoView.todoButton setTitle:@"Mark As In Progress" forState:UIControlStateHighlighted];
+		[self.todoView.todoStatus setText:@"Complete"];
+	} else {
+		[self.todoView.todoButton setTitle:@"Mark As Complete" forState:UIControlStateNormal];
+		[self.todoView.todoButton setTitle:@"Mark As Complete" forState:UIControlStateHighlighted];
+		[self.todoView.todoStatus setText:@"In Progress"];
+	}
+}
+
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+	todoAppDelegate *appDelegate = (todoAppDelegate *)[[UIApplication sharedApplication] delegate];
+	Todo *todo = (Todo *)[appDelegate.todos objectAtIndex:indexPath.row];
+	
+	if (editingStyle == UITableViewCellEditingStyleDelete) {
+		[appDelegate removeTodo:todo];
+		// Delete the row from the data source
+		[tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:YES];
+		//[self.tableView reloadData];
+	}	
+}
+
+
+
+/*
+ Override if you support conditional editing of the list
+ - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the specified item to be editable.
+ return YES;
+ }
+ */
+
+
+/*
+ Override if you support rearranging the list
+ - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+ }
+ */
+
+
+/*
+ Override if you support conditional rearranging of the list
+ - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+ // Return NO if you do not want the item to be re-orderable.
+ return YES;
+ }
+ */ 
+
+
+
+/*
+- (void)viewWillAppear:(BOOL)animated {
+	[self.tableView reloadData];
+	[super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+	[super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+}
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+	// Return YES for supported orientations
+	return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+	// Release anything that's not essential, such as cached data
+}
+
+@end
+
+*/
